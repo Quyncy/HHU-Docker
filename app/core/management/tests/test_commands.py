b53ca9d462f7592bbf6166fbbfeaa10fb7ab0119
Quynh <quynhmai@gmx.de>
2022-07-14 23:00:29 +0200

    GithubAction

--- app/core/management/tests/test_commands.py
+++ app/core/management/tests/test_commands.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -19,4 +20,3 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-        
