1f3918dcbff6de7ab28ef3cd38fedcaa387abe7a
Quynh <quynhmai@gmx.de>
2022-07-15 13:03:48 +0200

    New changes

--- app/core/management/tests/test_commands.py
+++ app/core/management/tests/test_commands.py
@@ -1,5 +1,6 @@
-
-
+"""
+Test custom Djanngo management
+"""
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg20pError
@@ -19,4 +20,15 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for databse when getting OperationalError"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patche_check.assert_called_with(database=['default'])
